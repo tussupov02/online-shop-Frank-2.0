ec4d02a7607309895a346cfc8518cc6eba052bd6
tussupov0209 <tussupov0209@gmail.com>
2024-04-02 10:41:11 +0600

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,44 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Hero from "./Components/Hero/Hero";
 import logo from "./Assets/logo.png";
-import Product from "./Components/Product/Product";
 import Footer from "./Components/Footer/Footer";
+import { BsInstagram } from "react-icons/bs";
+import { HiOutlinePhone } from "react-icons/hi";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <div className="main_header">
-          <div className="main_header_left">
-            <Link to='/'>
-            <img src={logo} alt="" />
-            </Link>
-            <p className="internet_shop">ИНТЕРНЕТ-МАГАЗИН</p>
-          </div>
-          <div className="main_header_right">
-            <a href="#">О магазине </a>
-            <a href="#">Доставка товаров </a>
-            <a href="#">Контакты</a>
-          </div>
+      <div className="main_header">
+        <div className="main_header_left">
+          <p>
+            <HiOutlinePhone id="numb"/>
+            +7 (705) 880-68-68
+          </p>
+          <a
+            className="inst"
+            href="https://www.instagram.com/frank.elite?igsh=ZnFhMjU4enNreDNx"
+          >
+            <BsInstagram style={{ fontSize: "32px" }} id="inst"/>
+            frank.elite
+          </a>
+          <img src={logo} alt="" />
         </div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero/>}/>
-          <Route path="/product" element={<Product/>}/>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+        <div className="main_header_right">
+          <a className="main_header_right_a" href="#">
+            О магазине{" "}
+          </a>
+          <a className="main_header_right_a" href="#">
+            Доставка товаров{" "}
+          </a>
+          <a className="main_header_right_a" href="#">
+            Контакты
+          </a>
+        </div>
+      </div>
+      <Header />
+      <Hero />
+      <Footer />
     </div>
   );
 }
--- frontend/src/Components/ReadyMade/ReadyMade.js
+++ frontend/src/Components/ReadyMade/ReadyMade.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./ReadyMade.css";
 
 function ReadyMade(props) {
   return (
     <div className="ready-made_kits_catalog">
-      <Link to="/" style={{textDecoration:'none', color:'#112038'}}>
       <div className="ready-made_content_box" id="ready-made">
         <div className="img">
           <img src={props.image} alt="" />
@@ -15,7 +13,6 @@ function ReadyMade(props) {
           <p>{props.content}</p>
         </div>
       </div>
-      </Link>
       <div className="ready-made_kits_price">
         <p>{props.price} ₸ </p>
         <button className="bascet">В корзину</button>
